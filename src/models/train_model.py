0a947dab7aafb9c4d132f7db318eca8d06d04be2
TalissaMoura <talissa.ayres@gmail.com>
2024-03-20 23:46:37 -0400

    apply feat selection option

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -12,7 +12,9 @@ from pathlib import Path
 
 def make_train(X_train:Union[np.array,pd.DataFrame],
                y_train:Union[np.array,pd.DataFrame],
-               config:Path):
+               config:Path,
+               use_fs:Union[True,False]=False,
+               ):
     
     with open(config,"r") as cfg_file:
         cfg=yaml.safe_load(cfg_file)
@@ -26,10 +28,26 @@ def make_train(X_train:Union[np.array,pd.DataFrame],
         ('cat', OneHotEncoder(), cat_features)
         ],
     )
+    if use_fs:
+        select_cols = cfg["model_parameters"]["fs_params"]["select_cols_arr"]
+        apply_fs = ColumnTransformer(
+            transformers=[
+                ("select_cols","passthrough",select_cols)
+            ]
+        )
+        # Define the pipeline
+        model_params = cfg["model_parameters"]
+        pipeline = Pipeline(steps=[
+            ('preprocessor', preprocessor),  # Preprocessing (imputation + one-hot encoding)
+            ('select_fs',apply_fs),
+            ('model', RandomForestClassifier(**model_params["fit_params"]))  # Model
+            ])
+
     # Define the pipeline
     model_params = cfg["model_parameters"]
     pipeline = Pipeline(steps=[
-    ('preprocessor', preprocessor),  # Preprocessing (imputation + one-hot encoding)
-    ('model', RandomForestClassifier(**model_params["fit_params"]))  # Model
-    ])
+            ('preprocessor', preprocessor),  # Preprocessing (imputation + one-hot encoding)
+            ('model', RandomForestClassifier(**model_params["fit_params"]))  # Model
+            ])
+        
     return pipeline.fit(X_train,y_train)
